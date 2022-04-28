0aaea207ce019c46b733ec91306b24f7f9fa6eae
Eric Stern <estern7@binghamton.edu>
2022-04-28 12:49:30 -0400

    printInOrder() written

--- BST.h
+++ BST.h
@@ -60,13 +60,26 @@ class BST {
             if(root == nullptr){
                 *root = val;
             }
-        }  
+        }
+
+        void printInOrder(Node target)
+        {
+            if (target->left != nullptr) //Explore the left child of TARGET
+            {
+                printInOrder(target.left); //Recursive call on left child
+            }
+
+            cout << target.data << endl; //Print the data in TARGET
+
+            if (target.right != nullptr) //Explore the right child of TARGET
+            {
+                printInOrder(target.right) //Recursive call on right child
+            }
+        }
+
     private:
         int size;
-        Node *root; 
-        Node *left;
-        Node *right; 
-
+        Node *root;
 };
 
 
