d5afd14b4fc30c4d1d08306f300b21763a28ae03
Eric Stern <estern7@binghamton.edu>
2022-05-03 12:41:11 -0400

    Added timekeeping function

--- main.cpp
+++ main.cpp
@@ -16,15 +16,29 @@ int main(int argc, char *argv[]){
     BST<int> bst; 
     BST<int> bstR; 
     ifstream file(argv[1]);
-     if (file.is_open()){
-        while(getline(file, line)){
-            //Comment
+    if (file.is_open()){
+        while(getline(file, line))
+        {
+            time_t now = time(nullptr); //Init time variable
+            time_t milliTime = now * 1000; //Convert to milliseconds
+
             bst.insert(stoi(line));
+
+            time_t later = time(nullptr); //Init later variable
+            later = later * 1000; //Convert to milliseconds
+            now = later - now; //Get the difference between the times
+            cout << now << endl; //Print the difference
+
+            now = time(nullptr); //Reset the time varaible
             bstR.insertRecursively(stoi(line));
+            later = time(nullptr);
+
+            now = later - now;
+            cout << now << endl; //Output the difference
         }
-     }
-     file.close();
-     bst.print();   
+    }
+    file.close();
+    bst.print();
     //  cout << bst.findRecursively(27);
     //  bstR.print();
 }
