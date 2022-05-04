32c3f2a6f26332e9b9d9ca19450d8634e914adae
Adam Cohen <acohen89@binghamton.edu>
2022-05-04 17:10:03 -0400

    Added finds

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 #include <iostream>
 #include <chrono>
 #include <fstream>
+#include <bits/stdc++.h>
 #include <sstream>
+#include <stdlib.h>     
+#include <time.h>       
 #include <vector>
 #include <time.h>
 #include <array>
@@ -14,6 +17,9 @@ using namespace std;
 #include "BST.h"
 
 int countValues(string basicString);
+
+#define NUM_RANDS 10000
+#define INT_RANGE 350000000
 
 int main(int argc, char *argv[]){
     string line;
@@ -41,8 +47,30 @@ int main(int argc, char *argv[]){
         }
     }
     stop = std::chrono::high_resolution_clock::now();
-    cout << "Recursive Time Taken: " << (double)std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() / 1000000 << " Seconds ";
+    cout << "Recursive Time Taken: " << (double)std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() / 1000000 << " Seconds " << endl; 
+
+    srand (time(NULL)); 
+    vector<int> randNums; 
+    for(int i = 0; i < NUM_RANDS; i++){    
+        randNums.push_back(rand() % INT_RANGE); 
+    }
+    start = std::chrono::high_resolution_clock::now(); 
+    for(int i = 0; i < NUM_RANDS; i++){
+        bst.find(randNums.at(i));
+    }
+    stop = std::chrono::high_resolution_clock::now();
+    cout << "Time Taken: " << (double)std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() / 1000000 << " Seconds " << endl; 
+    
+    start = std::chrono::high_resolution_clock::now(); 
+    for(int i = 0; i < NUM_RANDS; i++){
+        bstR.findRecursively(randNums.at(i));
+    }
+    stop = std::chrono::high_resolution_clock::now();
+    cout << "Recursive Time Taken: " << (double)std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() / 1000000 << " Seconds " << endl; 
 }
+
+
+
 
 /**
  * Counts the number of input line2s in a file
